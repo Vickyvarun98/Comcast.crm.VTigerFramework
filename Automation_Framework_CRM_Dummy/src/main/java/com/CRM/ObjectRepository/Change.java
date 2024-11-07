964bdc7622d887eb3bf40e1c7bb6244cb512e8c9
vino <vino@User.mshome.net>
2024-11-07 10:45:18 +0530

    Changes Done

--- Automation_Framework_CRM_Dummy/src/main/java/com/CRM/ObjectRepository/Change.java
+++ Automation_Framework_CRM_Dummy/src/main/java/com/CRM/ObjectRepository/Change.java
@@ -1,5 +1,11 @@
 package com.CRM.ObjectRepository;
 
 public class Change {
+	
+	public static void main(String[] args) {
+		System.out.println("Hii Asha Gaaru");
+		System.out.println("Good Morning");
+		System.out.println("Breakfast Tinnava?");
+	}
 
 }
